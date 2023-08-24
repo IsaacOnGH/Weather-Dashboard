8396852a5c63c078813301bae962838fe033dfbd
IsaacOnGH <isaac.melanson@yahoo.com>
2023-08-24 17:31:18 -0500

    Adding the index.html, gitignore, .env file, basic styles, and a script.js set up consts.

---
+++ script.js
@@ -0,0 +1,7 @@
+const apiKey = process.env.OPENWEATHERMAP_API_KEY;
+const searchForm = document.getElementById('search-form');
+const cityInput = document.getElementById('city-input');
+const currentWeatherSection = document.getElementById('current-weather');
+const forecastSection = document.getElementById('forecast');
+const searchHistory = [];
+
