25f26c8bdf51a820b99550ab7689a065e2c21c9c
IsaacOnGH <isaac.melanson@yahoo.com>
2023-08-24 17:36:41 -0500

    Adding a search form for cities

--- script.js
+++ script.js
@@ -5,3 +5,27 @@ const currentWeatherSection = document.getElementById('current-weather');
 const forecastSection = document.getElementById('forecast');
 const searchHistory = [];
 
+searchForm.addEventListener('submit', async function (event) {
+    event.preventDefault();
+    const city = cityInput.value.trim();
+
+    if (city === '') {
+        return;
+    }
+
+    // Fetch current weather data
+    const currentWeatherData = await fetchCurrentWeather(city);
+    displayCurrentWeather(currentWeatherData);
+
+    // Fetch forecast data
+    const forecastData = await fetchForecast(city);
+    displayForecast(forecastData);
+
+    // Add city to search history
+    if (!searchHistory.includes(city)) {
+        searchHistory.push(city);
+        updateSearchHistory();
+    }
+
+    cityInput.value = ''; // Clear input
+});
